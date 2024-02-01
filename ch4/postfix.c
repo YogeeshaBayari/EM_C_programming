ad8e6857a12fa7aa1cac1c47ee5f426662fd229c
YogeeshaBayari <70871926+YogeeshaBayari@users.noreply.github.com>
2024-02-01 23:58:14 +0530

    Update postfix.c

--- ch4/postfix.c
+++ ch4/postfix.c
@@ -72,3 +72,92 @@ int main() {
     return 0;
 }
 
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+2nd version
+
+<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX_STACK_SIZE 100
+
+double stack[MAX_STACK_SIZE];
+int top = -1;
+
+void push(double n) {
+    if (top == MAX_STACK_SIZE - 1) {
+        printf("Error: Stack is full\n");
+        exit(EXIT_FAILURE);
+    } else {
+        stack[++top] = n;
+    }
+}
+
+double pop() {
+    if (top == -1) {
+        printf("Error: Stack is empty\n");
+        exit(EXIT_FAILURE);
+    } else {
+        return stack[top--];
+    }
+}
+
+double eval(char *expression) {
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (isdigit(expression[i]) || (expression[i] == '.' && isdigit(expression[i + 1]))) {
+            push(atof(&expression[i]));
+            while (isdigit(expression[i]) || expression[i] == '.') {
+                i++;
+            }
+            i--;  // Move back one position to handle the increment in the loop
+        } else {
+            double op2 = pop();
+            double op1 = pop();
+
+            switch (expression[i]) {
+                case '+':
+                    push(op1 + op2);
+                    break;
+                case '-':
+                    push(op1 - op2);
+                    break;
+                case '*':
+                    push(op1 * op2);
+                    break;
+                case '/':
+                    if (op2 != 0) {
+                        push(op1 / op2);
+                    } else {
+                        printf("Error: Division by zero\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                default:
+                    printf("Error: Invalid Expression\n");
+                    exit(EXIT_FAILURE);
+            }
+        }
+    }
+    return pop();
+}
+
+int main() {
+    char postfix[100];
+
+    while (1) {
+        printf("Enter a postfix expression (or 'q' to quit): ");
+        scanf("%s", postfix);
+
+        if (postfix[0] == 'q' && postfix[1] == '\0') {
+            printf("Exiting the program. Goodbye!\n");
+            break;
+        }
+
+        double result = eval(postfix);
+        printf("Result: %.8lf\n", result);
+    }
+
+    return 0;
+}
